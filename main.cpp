f0cec77951ffc53b0ef8c092d31508bad3447723
MurilloYonamine <murillo.gyonamine@gmail.com>
2025-12-21 02:06:35 -0300

    feat: configure CMake and add CI workflow for Crow application

--- main.cpp
+++ main.cpp
@@ -4,12 +4,8 @@ int main() {
 	crow::SimpleApp app;
 
 	CROW_ROUTE(app, "/")([]() {
-		auto html = crow::mustache::load_text("index.html");
-		return html;
-	});
-
-	CROW_ROUTE(app, "/add/<int>/<int>")([](int a, int b) {
-		return std::to_string(a + b);
+		auto index = crow::mustache::load_text("index.html");
+		return index;
 	});
 
 	app.port(18080).multithreaded().run();
